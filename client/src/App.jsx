b087021299f097d835182201341172fa8a947177
davidtc8 <davidtorresc8@gmail.com>
2024-04-09 13:56:30 -0600

    New changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Main() {
   return (
-    <div className='text-3xl'>Shanandra sexo</div>
+    <div className='text-3xl'>Shanandra mi novia</div>
   )
 }
